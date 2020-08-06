f751feea3e52a380845b490b288a682e9fbb34b5
Jessica <reynolds.jessann@gmail.com>
2020-08-06 19:28:42 -0400

    Add code to handle post for logos

--- src/routes/logosRoutes.js
+++ src/routes/logosRoutes.js
@@ -6,4 +6,14 @@ router.get('/', async (req, res) => {
   res.send(await getLogos());
 });
 
+router.post('/', async (apiRequest, apiResponse) => {
+  const newLogo = apiRequest.body;
+  await createLogo(newLogo);
+  apiResponse.send({
+    message: 'New logo created.',
+    allLogos: await getLogos(),
+    thanks: true
+  });
+});
+
 module.exports = router;
